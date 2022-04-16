fb418b5bcf5d81da712940d77818d11255d77db7
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-04-16 13:19:20 -0400

    Updat app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const generatePage =() => 'Name: Jane, Github: Janehub';  //string
+
+console.log(generatePage());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//================================================================
+// const profileDataArgs = process.argv.slice(2);
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+//   // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
