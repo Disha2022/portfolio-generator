a52bcb811b20c3d6918bae3cf83abfa79d32e04c
Disha2022 <99584117+Disha2022@users.noreply.github.com>
2022-04-21 12:39:12 -0400

    created package.json and added inqire

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-// const profileDataArgs = process.argv.slice(2, process.argv.length);
-const fs = require ('fs')
-const generatePage = require('./src/page-template.js');
-const profileDataArgs = process.argv.slice(2); //?????????????
-console.log(profileDataArgs);
-const [name, github] = profileDataArgs; //??????????????///
+const inquirer = require('inquirer')
+inquirer.prompt([
 
+{
+  type:'input',
+  name:'name',
+  message: 'what is your name?'
+}
 
-fs.writeFile('index.html', generatePage(name, github),err =>{
-  if (err) {console.log(err)}
-  
-  console.log('Portfolio complete!Check out index.html to see the output!');
-});
+])
+.then (answers=> console.log(answers))
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
 
 
